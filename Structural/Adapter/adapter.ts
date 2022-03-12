2be6915f282b8dbf5048cd7dd2b149fc44a3e556
yevgeller <yevgeller@gmail.com>
2022-03-11 21:09:35 -0600

    Adding JSON input file

--- Structural/Adapter/adapter.ts
+++ Structural/Adapter/adapter.ts
@@ -1,11 +1,17 @@
 class CSVProvider {
-    public getData() : string {
-        const header = "FirstName,LastName,Age";
-        const line1 = "John,Smith,25";
-        const line2 = "Mary,Smith,25";
-        const line3 = "Jacob,Smith,3";
-        const CRLF = "\n";
-        const interimResult = [header, line1, line2, line3];
-        return interimResult.join(CRLF);
-    }
+  public getData(): string {
+    const header = "FirstName,LastName,Age";
+    const line1 = "John,Smith,25";
+    const line2 = "Mary,Smith,25";
+    const line3 = "Jacob,Smith,3";
+    const CRLF = "\n";
+    const interimResult = [header, line1, line2, line3];
+    return interimResult.join(CRLF);
+  }
+}
+
+class JSONProcessor {
+  public doSomethingWithJSON(json: string): void {
+    console.log(JSON.stringify(json, null, 2));
+  }
 }
