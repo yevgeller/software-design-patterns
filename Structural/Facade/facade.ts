590145336fd981d1e0b84c88dc949ab1ac4f6761
yevgeller <yevgeller@gmail.com>
2022-04-19 21:03:16 -0500

    Commented out kitchen example for Facade

--- Structural/Facade/facade.ts
+++ Structural/Facade/facade.ts
@@ -1,31 +1,31 @@
-class Refrigerator {
-  items = [] as any;
-  open = () => console.log("refrigerator is open");
-  close = () => console.log("refrigerator is closed");
-  get = (item: string) => {
-    if (this.items.indexOf(item) >= 0) return `retrieved ${item}`;
-    else return `no ${item} in the fridge`;
-  };
+// class Refrigerator {
+//   items = [] as any;
+//   open = () => console.log("refrigerator is open");
+//   close = () => console.log("refrigerator is closed");
+//   get = (item: string) => {
+//     if (this.items.indexOf(item) >= 0) return `retrieved ${item}`;
+//     else return `no ${item} in the fridge`;
+//   };
 
-  constructor() {
-    this.items = ["eggs", "bacon", "sausage", "orange juice", "milk", "cream"];
-  }
-}
+//   constructor() {
+//     this.items = ["eggs", "bacon", "sausage", "orange juice", "milk", "cream"];
+//   }
+// }
 
-class Chef {
-  comeInToWork = () => console.log("grumble grumble, I have arrived");
-  leave = () => console.log("see you tomorrow");
-  washHands = () =>
-    console.log("I am going to go wash my hands, back in a minute");
-}
+// class Chef {
+//   comeInToWork = () => console.log("grumble grumble, I have arrived");
+//   leave = () => console.log("see you tomorrow");
+//   washHands = () =>
+//     console.log("I am going to go wash my hands, back in a minute");
+// }
 
-class Stove {
-  mediumHeat = () => console.log("stove is on medium heat");
-  highHeat = () => console.log("stove is on high heat");
-  lowHeat = () => console.log("stove is on low heat");
-}
+// class Stove {
+//   mediumHeat = () => console.log("stove is on medium heat");
+//   highHeat = () => console.log("stove is on high heat");
+//   lowHeat = () => console.log("stove is on low heat");
+// }
 
-//maybe redo this as a movie projection or a play start
+// //maybe redo this as a movie projection or a play start
 class SoundBooth {
   setUpSound = (): void => console.log("sound set up");
   tearDownSound = (): void => console.log("sound system put away");
