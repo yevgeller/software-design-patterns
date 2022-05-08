23d13665755f35ef90dad086c51eb6b426be1b0a
yevgeller <yevgeller@gmail.com>
2022-05-08 10:22:57 -0500

    Iterator2 done

--- Behavioral/Iterator/iterator2.js
+++ Behavioral/Iterator/iterator2.js
@@ -3,7 +3,11 @@ var IngredientIterator;
     var Ingredient = /** @class */ (function () {
         function Ingredient(name, isDry, unitOfMeasure, units) {
             var _this = this;
-            this.displayIngredient = function () { return "".concat(_this.name, " (").concat(_this.isDry ? 'dry' : 'wet', "), ").concat(_this.units, " ").concat(_this.unitOfMeasure.toLocaleLowerCase() === 'unit' ? '' : _this.unitOfMeasure); };
+            this.displayIngredient = function () {
+                return "".concat(_this.name, " (").concat(_this.isDry ? "dry" : "wet", "), ").concat(_this.units, " ").concat(_this.unitOfMeasure.toLocaleLowerCase() === "unit"
+                    ? ""
+                    : _this.unitOfMeasure);
+            };
             this.name = name;
             this.isDry = isDry;
             this.unitOfMeasure = unitOfMeasure;
@@ -20,11 +24,11 @@ var IngredientIterator;
         Recipe.prototype.printRecipe = function () {
             console.log(this.dishName.toUpperCase());
             var ingrenum = this.ingredients.getEnumerator();
-            console.log('\nINGREDIENTS:');
+            console.log("\nINGREDIENTS:");
             while (ingrenum.hasNext()) {
                 console.log(ingrenum.next().displayIngredient());
             }
-            console.log('\nDIRECTIONS: ');
+            console.log("\nDIRECTIONS: ");
             this.directions.forEach(function (d) { return console.log(d); });
         };
         Recipe.prototype.cookRecipe = function () {
@@ -57,30 +61,51 @@ var IngredientIterator;
             this.currentItem = this.allIngredients[this.currentIndex];
             return this.currentItem;
         };
-        IngredientEnumerator.prototype.hasNext = function () { return this.currentIndex < this.allIngredients.length - 1; };
+        IngredientEnumerator.prototype.hasNext = function () {
+            return this.currentIndex < this.allIngredients.length - 1;
+        };
         IngredientEnumerator.prototype.sortIngredientsByTypeThenName = function () {
             var _a;
             var sorted = this.allIngredients.filter(function (x) { return x.isDry == true; });
-            sorted.sort(function (a, b) { return (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0); });
+            sorted.sort(function (a, b) { return (a.name > b.name ? 1 : a.name < b.name ? -1 : 0); });
             var wetIngredients = this.allIngredients.filter(function (x) { return x.isDry === false; });
-            wetIngredients.sort(function (a, b) { return (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0); });
+            wetIngredients.sort(function (a, b) {
+                return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+            });
             sorted.push.apply(sorted, wetIngredients);
             this.allIngredients.length = 0;
             (_a = this.allIngredients).push.apply(_a, sorted);
         };
         return IngredientEnumerator;
     }());
-    var ingrFlour = new Ingredient('flour', true, 'cup', '1.5');
-    var ingrSugar = new Ingredient('sugar', true, 'cup', '0.5');
-    var ingrBakingPowder = new Ingredient('baking powder', true, 'tsp', '1.5');
-    var ingrSalt = new Ingredient('salt', true, 'tsp', '0.5');
-    var ingrOil = new Ingredient('oil', false, 'cup', '1/3');
-    var ingrVanilla = new Ingredient('vanilla extract', false, 'tbsp', '2');
-    var ingrRicotta = new Ingredient('ricotta cheese', false, 'cup', '1.5');
-    var ingrMilk = new Ingredient('milk', false, 'tbsp', '5');
-    var ingrEgg = new Ingredient('egg', false, 'unit', '1');
-    var ingrBlueberries = new Ingredient('blueberries', false, 'cup', '1/3');
-    var arrayOfIngre = [ingrBakingPowder, ingrBlueberries, ingrEgg, ingrFlour, ingrMilk, ingrOil, ingrRicotta, ingrSalt, ingrSugar, ingrVanilla];
-    var muffins = new Recipe('Ricotta blueberry muffins', arrayOfIngre, ['mix dry ingredients', 'mix wet ingredients', 'mix together', 'put in muffin pan', 'bake at 375 for 32 minutes or until ready']);
+    var ingrFlour = new Ingredient("flour", true, "cup", "1.5");
+    var ingrSugar = new Ingredient("sugar", true, "cup", "0.5");
+    var ingrBakingPowder = new Ingredient("baking powder", true, "tsp", "1.5");
+    var ingrSalt = new Ingredient("salt", true, "tsp", "0.5");
+    var ingrOil = new Ingredient("oil", false, "cup", "1/3");
+    var ingrVanilla = new Ingredient("vanilla extract", false, "tbsp", "2");
+    var ingrRicotta = new Ingredient("ricotta cheese", false, "cup", "1.5");
+    var ingrMilk = new Ingredient("milk", false, "tbsp", "5");
+    var ingrEgg = new Ingredient("egg", false, "unit", "1");
+    var ingrBlueberries = new Ingredient("blueberries", false, "cup", "1/3");
+    var arrayOfIngre = [
+        ingrBakingPowder,
+        ingrBlueberries,
+        ingrEgg,
+        ingrFlour,
+        ingrMilk,
+        ingrOil,
+        ingrRicotta,
+        ingrSalt,
+        ingrSugar,
+        ingrVanilla,
+    ];
+    var muffins = new Recipe("Ricotta blueberry muffins", arrayOfIngre, [
+        "mix dry ingredients",
+        "mix wet ingredients",
+        "mix together",
+        "put in muffin pan",
+        "bake at 375 for 32 minutes or until ready",
+    ]);
     muffins.printRecipe();
 })(IngredientIterator || (IngredientIterator = {}));
--- Behavioral/Iterator/iterator2.ts
+++ Behavioral/Iterator/iterator2.ts
@@ -1,109 +1,143 @@
 namespace IngredientIterator {
-    interface IIngredientEnumerable {
-        getEnumerator(): IIngredientIterator;
+  interface IIngredientEnumerable {
+    getEnumerator(): IIngredientIterator;
+  }
+
+  class Ingredient {
+    name: string;
+    isDry: boolean;
+    unitOfMeasure: string;
+    units: string;
+    constructor(
+      name: string,
+      isDry: boolean,
+      unitOfMeasure: string,
+      units: string
+    ) {
+      this.name = name;
+      this.isDry = isDry;
+      this.unitOfMeasure = unitOfMeasure;
+      this.units = units;
     }
 
-    class Ingredient {
-        name: string;
-        isDry: boolean;
-        unitOfMeasure: string;
-        units: string;
-        constructor(
-            name: string,
-            isDry: boolean,
-            unitOfMeasure: string,
-            units: string
-        ) {
-            this.name = name;
-            this.isDry = isDry;
-            this.unitOfMeasure = unitOfMeasure;
-            this.units = units;
-        }
+    displayIngredient = () =>
+      `${this.name} (${this.isDry ? "dry" : "wet"}), ${this.units} ${
+        this.unitOfMeasure.toLocaleLowerCase() === "unit"
+          ? ""
+          : this.unitOfMeasure
+      }`;
+  }
 
-        displayIngredient = () => `${this.name} (${this.isDry ? 'dry' : 'wet'}), ${this.units} ${this.unitOfMeasure.toLocaleLowerCase() === 'unit' ? '' : this.unitOfMeasure}`
+  class Recipe {
+    ingredients: IngredientsCollection;
+    directions: Array<string>;
+    dishName: string;
+    constructor(
+      dishName: string,
+      ingredients: Array<Ingredient>,
+      directions: Array<string>
+    ) {
+      this.dishName = dishName;
+      this.ingredients = new IngredientsCollection(ingredients);
+      this.directions = directions;
     }
-
-    class Recipe {
-        ingredients: IngredientsCollection;
-        directions: Array<string>;
-        dishName: string;
-        constructor(dishName: string, ingredients: Array<Ingredient>, directions: Array<string>) {
-            this.dishName = dishName;
-            this.ingredients = new IngredientsCollection(ingredients);
-            this.directions = directions;
-        }
-        printRecipe(): void {
-            console.log(this.dishName.toUpperCase());
-            let ingrenum = this.ingredients.getEnumerator();
-            console.log('\nINGREDIENTS:')
-            while (ingrenum.hasNext()) {
-                console.log(ingrenum.next().displayIngredient());
-            }
-            console.log('\nDIRECTIONS: ')
-            this.directions.forEach((d) => console.log(d))
-        }
-        cookRecipe(): void {
-            //lol not this time
-        }
+    printRecipe(): void {
+      console.log(this.dishName.toUpperCase());
+      let ingrenum = this.ingredients.getEnumerator();
+      console.log("\nINGREDIENTS:");
+      while (ingrenum.hasNext()) {
+        console.log(ingrenum.next().displayIngredient());
+      }
+      console.log("\nDIRECTIONS: ");
+      this.directions.forEach((d) => console.log(d));
     }
-
-    interface IIngredientIterator {
-        next(): Ingredient;
-        hasNext(): boolean;
-        currentItem: Ingredient;
+    cookRecipe(): void {
+      //lol not this time
     }
+  }
 
-    class IngredientsCollection implements IIngredientEnumerable {
-        private allIngredients: Array<Ingredient>;
-        constructor(allIngredients: Array<Ingredient>) {
-            if (allIngredients === undefined || allIngredients.length === 0)
-                throw new Error("Cannot iterate over empty ingredient collection")
-            this.allIngredients = allIngredients;
-        }
-        getEnumerator(): IIngredientIterator {
-            return new IngredientEnumerator(this.allIngredients);
-        }
+  interface IIngredientIterator {
+    next(): Ingredient;
+    hasNext(): boolean;
+    currentItem: Ingredient;
+  }
+
+  class IngredientsCollection implements IIngredientEnumerable {
+    private allIngredients: Array<Ingredient>;
+    constructor(allIngredients: Array<Ingredient>) {
+      if (allIngredients === undefined || allIngredients.length === 0)
+        throw new Error("Cannot iterate over empty ingredient collection");
+      this.allIngredients = allIngredients;
     }
-
-    class IngredientEnumerator implements IIngredientIterator {
-        private allIngredients: Array<Ingredient>;
-        private currentIndex: number;
-        currentItem: Ingredient;
-        constructor(allIngredients: Array<Ingredient>) {
-            this.allIngredients = allIngredients;
-            this.sortIngredientsByTypeThenName();
-            this.currentIndex = 0;
-            this.currentItem = allIngredients[0]
-        }
-        next(): Ingredient {
-            if (!this.hasNext()) return null;
-            this.currentIndex++;
-            this.currentItem = this.allIngredients[this.currentIndex];
-            return this.currentItem;
-        }
-        hasNext(): boolean { return this.currentIndex < this.allIngredients.length - 1 }
-        private sortIngredientsByTypeThenName() {
-            let sorted = this.allIngredients.filter(x => x.isDry == true) as Ingredient[];
-            sorted.sort((a, b) => (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0));
-            let wetIngredients = this.allIngredients.filter(x => x.isDry === false) as Array<Ingredient>;
-            wetIngredients.sort((a, b) => (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0));
-            sorted.push(...wetIngredients)
-            this.allIngredients.length = 0;
-            this.allIngredients.push(...sorted);
-        }
+    getEnumerator(): IIngredientIterator {
+      return new IngredientEnumerator(this.allIngredients);
     }
-    let ingrFlour = new Ingredient('flour', true, 'cup', '1.5');
-    let ingrSugar = new Ingredient('sugar', true, 'cup', '0.5')
-    let ingrBakingPowder = new Ingredient('baking powder', true, 'tsp', '1.5')
-    let ingrSalt = new Ingredient('salt', true, 'tsp', '0.5')
-    let ingrOil = new Ingredient('oil', false, 'cup', '1/3')
-    let ingrVanilla = new Ingredient('vanilla extract', false, 'tbsp', '2')
-    let ingrRicotta = new Ingredient('ricotta cheese', false, 'cup', '1.5')
-    let ingrMilk = new Ingredient('milk', false, 'tbsp', '5')
-    let ingrEgg = new Ingredient('egg', false, 'unit', '1')
-    let ingrBlueberries = new Ingredient('blueberries', false, 'cup', '1/3')
-    let arrayOfIngre = [ingrBakingPowder, ingrBlueberries, ingrEgg, ingrFlour, ingrMilk, ingrOil, ingrRicotta, ingrSalt, ingrSugar, ingrVanilla] as Array<Ingredient>
+  }
 
-    let muffins = new Recipe('Ricotta blueberry muffins', arrayOfIngre, ['mix dry ingredients', 'mix wet ingredients', 'mix together', 'put in muffin pan', 'bake at 375 for 32 minutes or until ready'])
-    muffins.printRecipe();
+  class IngredientEnumerator implements IIngredientIterator {
+    private allIngredients: Array<Ingredient>;
+    private currentIndex: number;
+    currentItem: Ingredient;
+    constructor(allIngredients: Array<Ingredient>) {
+      this.allIngredients = allIngredients;
+      this.sortIngredientsByTypeThenName();
+      this.currentIndex = 0;
+      this.currentItem = allIngredients[0];
+    }
+    next(): Ingredient {
+      if (!this.hasNext()) return null;
+      this.currentIndex++;
+      this.currentItem = this.allIngredients[this.currentIndex];
+      return this.currentItem;
+    }
+    hasNext(): boolean {
+      return this.currentIndex < this.allIngredients.length - 1;
+    }
+    private sortIngredientsByTypeThenName() {
+      let sorted = this.allIngredients.filter(
+        (x) => x.isDry == true
+      ) as Ingredient[];
+      sorted.sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0));
+      let wetIngredients = this.allIngredients.filter(
+        (x) => x.isDry === false
+      ) as Array<Ingredient>;
+      wetIngredients.sort((a, b) =>
+        a.name > b.name ? 1 : a.name < b.name ? -1 : 0
+      );
+      sorted.push(...wetIngredients);
+      this.allIngredients.length = 0;
+      this.allIngredients.push(...sorted);
+    }
+  }
+  let ingrFlour = new Ingredient("flour", true, "cup", "1.5");
+  let ingrSugar = new Ingredient("sugar", true, "cup", "0.5");
+  let ingrBakingPowder = new Ingredient("baking powder", true, "tsp", "1.5");
+  let ingrSalt = new Ingredient("salt", true, "tsp", "0.5");
+  let ingrOil = new Ingredient("oil", false, "cup", "1/3");
+  let ingrVanilla = new Ingredient("vanilla extract", false, "tbsp", "2");
+  let ingrRicotta = new Ingredient("ricotta cheese", false, "cup", "1.5");
+  let ingrMilk = new Ingredient("milk", false, "tbsp", "5");
+  let ingrEgg = new Ingredient("egg", false, "unit", "1");
+  let ingrBlueberries = new Ingredient("blueberries", false, "cup", "1/3");
+  let arrayOfIngre = [
+    ingrBakingPowder,
+    ingrBlueberries,
+    ingrEgg,
+    ingrFlour,
+    ingrMilk,
+    ingrOil,
+    ingrRicotta,
+    ingrSalt,
+    ingrSugar,
+    ingrVanilla,
+  ] as Array<Ingredient>;
+
+  let muffins = new Recipe("Ricotta blueberry muffins", arrayOfIngre, [
+    "mix dry ingredients",
+    "mix wet ingredients",
+    "mix together",
+    "put in muffin pan",
+    "bake at 375 for 32 minutes or until ready",
+  ]);
+  muffins.printRecipe();
 }
