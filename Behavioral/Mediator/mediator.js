29f1bbbcad7adf02d88962e58c47caa11b5ffe9f
yevgeller <yevgeller@gmail.com>
2022-05-10 08:26:08 -0500

    Mediator update

---
+++ Behavioral/Mediator/mediator.js
@@ -0,0 +1,120 @@
+var Mother = /** @class */ (function () {
+    function Mother() {
+    }
+    Mother.prototype.saySomethingToDaughter = function () {
+        throw new Error("Method not implemented.");
+    };
+    Mother.prototype.saySomethingToSpouse = function () {
+        throw new Error("Method not implemented.");
+    };
+    Mother.prototype.saySomethingToSon = function () { };
+    return Mother;
+}());
+var Father = /** @class */ (function () {
+    function Father() {
+    }
+    Father.prototype.saySomethingToDaughter = function () {
+        throw new Error("Method not implemented.");
+    };
+    Father.prototype.saySomethingToSpouse = function () {
+        throw new Error("Method not implemented.");
+    };
+    Father.prototype.saySomethingToSon = function () { };
+    return Father;
+}());
+var Son = /** @class */ (function () {
+    function Son() {
+    }
+    Son.prototype.saySomethingToMother = function () {
+        throw new Error("Method not implemented.");
+    };
+    Son.prototype.saySomethingToFather = function () {
+        throw new Error("Method not implemented.");
+    };
+    Son.prototype.saySomethingToOtherSibling = function () {
+        throw new Error("Method not implemented.");
+    };
+    return Son;
+}());
+var Daughter = /** @class */ (function () {
+    function Daughter() {
+    }
+    Daughter.prototype.saySomethingToMother = function () {
+        throw new Error("Method not implemented.");
+    };
+    Daughter.prototype.saySomethingToFather = function () {
+        throw new Error("Method not implemented.");
+    };
+    Daughter.prototype.saySomethingToOtherSibling = function () {
+        throw new Error("Method not implemented.");
+    };
+    return Daughter;
+}());
+var FamilyMember = /** @class */ (function () {
+    function FamilyMember(name, position, room) {
+        this.name = name;
+        this.position = position;
+        this.room = room;
+    }
+    FamilyMember.prototype.getName = function () {
+        return this.name;
+    };
+    FamilyMember.prototype.getPosition = function () {
+        return this.position;
+    };
+    FamilyMember.prototype.send = function (message) {
+        console.log("".concat(this.name, " sent ").concat(message));
+        this.room.notify(message, this.name, false);
+    };
+    FamilyMember.prototype.sendPrivate = function (message, to) {
+        this.room.notifyDirectly(this.name, message, to);
+    };
+    FamilyMember.prototype.handleNotification = function (message, from, isPrivate) {
+        if (!isPrivate) {
+            console.log("".concat(this.position, " ").concat(this.name, " received message ").concat(message, " from ").concat(from));
+        }
+        else {
+            console.log("".concat(this.position, " ").concat(this.name, " received a PRIVATE message ").concat(message, " from ").concat(from));
+        }
+    };
+    FamilyMember.prototype.setChatRoom = function (room) {
+        this.room = room;
+    };
+    return FamilyMember;
+}());
+var ChatRoom = /** @class */ (function () {
+    function ChatRoom() {
+        this.recipients = [];
+    }
+    ChatRoom.prototype.notifyDirectly = function (from, message, name) {
+        var recipient = this.recipients.filter(function (r) { return r.getName() === name; });
+        if (recipient && recipient.length > 0 && recipient[0] != undefined) {
+            recipient[0].handleNotification(message, from, true);
+        }
+        else {
+            throw new Error("Recipient ".concat(name, " is not registered"));
+        }
+    };
+    ChatRoom.prototype.notify = function (message, from, isPrivate) {
+        this.recipients.forEach(function (r) { return r.handleNotification(message, from, false); });
+        console.log();
+    };
+    ChatRoom.prototype.register = function (person) {
+        this.recipients.push(person); //check for duplicates
+    };
+    ChatRoom.prototype.registerInBulk = function (people) {
+        var _a;
+        var _this = this;
+        (_a = this.recipients).push.apply(_a, people);
+        this.recipients.forEach(function (r) { return r.setChatRoom(_this); });
+    };
+    return ChatRoom;
+}());
+var cha = new ChatRoom();
+var fa = new FamilyMember("John", "father", cha);
+var ma = new FamilyMember("Mary", "mother", cha);
+var so = new FamilyMember("Jack", "son", cha);
+var da = new FamilyMember("Jill", "daughter", cha);
+cha.registerInBulk([fa, ma, so, da]);
+fa.send("hi!");
+da.sendPrivate("hello!", "Jack");
--- Behavioral/Mediator/mediator.ts
+++ Behavioral/Mediator/mediator.ts
@@ -57,12 +57,15 @@ class Daughter implements IChildCommunicator {
 }
 //------------------------- Mediator:
 interface IFamilyChatRoom {
-  notify(from: string, message: string): void;
   register(person: IFamilyMember): void;
+  notify(from: string, message: string, isPrivate: boolean): void;
+  notifyDirectly(from: string, message: string, name: string): void;
 }
 
 interface IFamilyMember {
   send(message: string): void;
+  sendPrivate(message: string, to: string): void;
+  receive(message: string, from: string, isPrivate: boolean): void;
   setChatRoom(room: IFamilyChatRoom): void;
   getName(): string;
   getPosition(): string;
@@ -71,10 +74,11 @@ interface IFamilyMember {
 class FamilyMember implements IFamilyMember {
   name: string;
   position: string;
-  room: IFamilyChatRoom;
-  constructor(name: string, position: string) {
+  room: IFamilyChatRoom; //everyone knows a single mediator
+  constructor(name: string, position: string, room: IFamilyChatRoom) {
     this.name = name;
     this.position = position;
+    this.room = room;
   }
 
   getName(): string {
@@ -86,7 +90,22 @@ class FamilyMember implements IFamilyMember {
   }
 
   send(message: string): void {
-    throw new Error("Method not implemented.");
+    console.log(`${this.name} sent ${message}`);
+    this.room.notify(message, this.name, false);
+  }
+  sendPrivate(message: string, to: string): void {
+    this.room.notifyDirectly(this.name, message, to);
+  }
+  receive(message: string, from: string, isPrivate: boolean) {
+    if (!isPrivate) {
+      console.log(
+        `${this.position} ${this.name} received message ${message} from ${from}`
+      );
+    } else {
+      console.log(
+        `${this.position} ${this.name} received a PRIVATE message ${message} from ${from}`
+      );
+    }
   }
   setChatRoom(room: IFamilyChatRoom): void {
     this.room = room;
@@ -94,27 +113,37 @@ class FamilyMember implements IFamilyMember {
 }
 
 class ChatRoom implements IFamilyChatRoom {
-  recipients: IFamilyMember[];
-
-  notify(from: string, message: string): void {
-    this.recipients.forEach((r) =>
-      console.log(
-        `${r.getPosition} ${r.getName} received message ${message} from ${from}`
-      )
-    );
+  recipients: IFamilyMember[]; //mediator knows about all
+  constructor() {
+    this.recipients = [];
+  }
+  notifyDirectly(from: string, message: string, name: string): void {
+    let recipient = this.recipients.filter((r) => r.getName() === name);
+    if (recipient && recipient.length > 0 && recipient[0] != undefined) {
+      recipient[0].receive(message, from, true);
+    } else {
+      throw new Error(`Recipient ${name} is not registered`);
+    }
+  }
+  notify(message: string, from: string, isPrivate: boolean): void {
+    this.recipients.forEach((r) => r.receive(message, from, false));
+    console.log();
   }
   register(person: IFamilyMember): void {
     this.recipients.push(person); //check for duplicates
   }
   registerInBulk(people: IFamilyMember[]): void {
     this.recipients.push(...people);
+    this.recipients.forEach((r) => r.setChatRoom(this));
   }
 }
 
-let fa = new FamilyMember("John", "father");
-let ma = new FamilyMember("Mary", "mother");
-let so = new FamilyMember("Jack", "son");
-let da = new FamilyMember("Jill", "daughter");
-
 let cha = new ChatRoom();
+let fa = new FamilyMember("John", "father", cha);
+let ma = new FamilyMember("Mary", "mother", cha);
+let so = new FamilyMember("Jack", "son", cha);
+let da = new FamilyMember("Jill", "daughter", cha);
+
 cha.registerInBulk([fa, ma, so, da]);
+fa.send("hi!");
+da.sendPrivate("hello!", "Jack");
