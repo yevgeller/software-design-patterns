d0c20cfd4ceb1926fd23b5465d6b3c86fa35ac81
yevgeller <yevgeller@gmail.com>
2022-05-11 09:13:41 -0500

    Memento hangman in progress

--- Behavioral/Memento/memento.js
+++ Behavioral/Memento/memento.js
@@ -3,26 +3,66 @@
 //   console.log(eh);
 //   eh = prompt("enter o to quit");
 // }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Game = /** @class */ (function () {
     function Game() {
         this.word = "SECRET";
         this.guesses = [];
         this.numberOfErrors = 0;
+        this.numberOfAllowedAttempts = 6;
+        console.log(this.currentPuzzleState());
+        this.supportUndo = false;
     }
     Game.prototype.processGuess = function (guess) {
+        if (guess.length !== 1) {
+            console.log("One letter at a time, please");
+            return;
+        }
+        guess = guess.toUpperCase();
+        if (!/^[a-zA-Z\-]+$/.test(guess)) {
+            console.log("alphabetic characters only, please");
+            return;
+        }
         if (this.guesses.indexOf(guess) >= 0) {
             console.log("already guessed ".concat(guess, ". Try again"));
             return;
         }
         this.guesses.push(guess);
-        if (this.word.indexOf(guess) >= 0)
+        var displayed = this.currentPuzzleState();
+        if (this.word.indexOf(guess) >= 0) {
             console.log("Good guess!");
-        else {
-            console.log("bad guess! This word contains no ".concat(guess, "."));
-            console.log("You have ".concat(6 - this.numberOfErrors, " guesses left"));
-            this.numberOfErrors++;
+            if (displayed.indexOf("_") < 0) {
+                console.log("Victory! With only ".concat(this.numberOfErrors, " errors!"));
+                this.gameOver = true;
+            }
         }
-        console.log(this.currentPuzzleState());
+        else {
+            this.numberOfErrors++;
+            console.log("bad guess! This word contains no ".concat(guess, "."));
+            console.log("You have ".concat(this.attemptsLeft(), " guesses left"));
+            if (this.attemptsLeft() === 0) {
+                this.gameOver = true;
+                console.log("You lost. The word was: ".concat(this.word.toUpperCase()));
+            }
+        }
+        console.log(displayed);
+    };
+    Game.prototype.attemptsLeft = function () {
+        return this.numberOfAllowedAttempts - this.numberOfErrors;
     };
     Game.prototype.currentPuzzleState = function () {
         var result = ""; //'_ _ _ _ _ _';
@@ -36,15 +76,50 @@ var Game = /** @class */ (function () {
         }
         return result;
     };
+    Game.prototype.gameIsOver = function () {
+        return this.gameOver;
+    };
     return Game;
 }());
-var g = new Game();
-g.guesses = ["S", "T", "E"];
-setTimeout(function () { return g.processGuess("A"); }, 500);
-setTimeout(function () { return g.processGuess("C"); }, 520);
-setTimeout(function () { return g.processGuess("H"); }, 510);
-setTimeout(function () { return g.processGuess("K"); }, 530);
-setTimeout(function () { return g.processGuess("L"); }, 550);
-setTimeout(function () { return g.processGuess("M"); }, 560);
-setTimeout(function () { return g.processGuess("N"); }, 570);
-setTimeout(function () { return g.processGuess("O"); }, 580);
+var Memento = /** @class */ (function () {
+    function Memento(guesses, numberOfErrors) {
+        this.guesses = guesses;
+        this.numberOfErrors = numberOfErrors;
+    }
+    return Memento;
+}());
+var GameWithUndo = /** @class */ (function (_super) {
+    __extends(GameWithUndo, _super);
+    function GameWithUndo() {
+        var _this = _super.call(this) || this;
+        _this.supportUndo = true;
+        console.log("undo supported!");
+        return _this;
+    }
+    GameWithUndo.prototype.createCheckPoint = function () {
+        return new Memento(this.guesses, this.numberOfErrors);
+    };
+    GameWithUndo.prototype.processCheckPoint = function (checkPoint) {
+        var _a;
+        this.guesses.length = 0;
+        (_a = this.guesses).push.apply(_a, checkPoint.guesses);
+        this.numberOfErrors = checkPoint.numberOfErrors;
+    };
+    return GameWithUndo;
+}(Game));
+var g = new Game(); //Originator
+var input = ".";
+// while (!g.gameIsOver()) {
+//   //this main program is the CareTaker
+//   input = prompt("enter a letter");
+//   console.log("Input: ", input);
+//   g.processGuess(input);
+// }
+var g2 = new GameWithUndo();
+var mementos = [];
+while (!g2.gameIsOver()) {
+    //this main program is the CareTaker
+    input = prompt("enter a letter, or '-' to undo");
+    console.log("Input: ", input);
+    g2.processGuess(input);
+}
--- Behavioral/Memento/memento.ts
+++ Behavioral/Memento/memento.ts
@@ -1,43 +1,63 @@
-// let eh = "a";
-// while (eh !== "o") {
-//   console.log(eh);
-//   eh = prompt("enter o to quit");
-// }
-
 class Game {
-  word: string;
+  private word: string;
   guesses: string[];
   numberOfErrors: number;
-  numberOfAllowedAttempts: number;
+  private numberOfAllowedAttempts: number;
+  private gameOver: boolean;
+  supportUndo: boolean;
 
   constructor() {
     this.word = "SECRET";
     this.guesses = [];
     this.numberOfErrors = 0;
     this.numberOfAllowedAttempts = 6;
+    console.log(this.currentPuzzleState());
+    this.supportUndo = false;
   }
 
   processGuess(guess: string) {
+    if (guess.length !== 1) {
+      console.log("One letter at a time, please"); //change this to a property
+      return;
+    }
+    guess = guess.toUpperCase();
+    if (!/^[a-zA-Z\-]+$/.test(guess)) {
+      console.log("alphabetic characters only, please");
+      return;
+    }
     if (this.guesses.indexOf(guess) >= 0) {
       console.log(`already guessed ${guess}. Try again`);
       return;
     }
+
     this.guesses.push(guess);
-    if (this.word.indexOf(guess) >= 0) console.log("Good guess!");
-    else {
-      console.log(`bad guess! This word contains no ${guess}.`);
-      console.log(
-        `You have ${
-          this.numberOfAllowedAttempts - this.numberOfErrors
-        } guesses left`
-      );
+    let displayed = this.currentPuzzleState() as string;
+
+    if (this.word.indexOf(guess) >= 0) {
+      console.log("Good guess!");
+      if (displayed.indexOf("_") < 0) {
+        console.log(`Victory! With only ${this.numberOfErrors} errors!`);
+        this.gameOver = true;
+      }
+    } else {
       this.numberOfErrors++;
+      console.log(
+        `bad guess! This word contains no ${guess}. You have ${this.attemptsLeft()} guesses left`
+      );
+      if (this.attemptsLeft() === 0) {
+        this.gameOver = true;
+        console.log(`You lost. The word was: ${this.word.toUpperCase()}`);
+      }
     }
-    console.log(this.currentPuzzleState());
+    console.log(displayed);
+  }
+
+  private attemptsLeft(): number {
+    return this.numberOfAllowedAttempts - this.numberOfErrors;
   }
 
   private currentPuzzleState(): string {
-    let result = ""; //'_ _ _ _ _ _';
+    let result = "";
     for (let i = 0; i < this.word.length; i++) {
       if (this.guesses.indexOf(this.word[i].toString()) >= 0) {
         result += this.word[i].toString().toUpperCase() + " ";
@@ -47,15 +67,60 @@ class Game {
     }
     return result;
   }
+
+  public gameIsOver(): boolean {
+    return this.gameOver;
+  }
 }
 
-let g = new Game();
-g.guesses = ["S", "T", "E"];
-setTimeout(() => g.processGuess("A"), 500);
-setTimeout(() => g.processGuess("C"), 520);
-setTimeout(() => g.processGuess("H"), 510);
-setTimeout(() => g.processGuess("K"), 530);
-setTimeout(() => g.processGuess("L"), 550);
-setTimeout(() => g.processGuess("M"), 560);
-setTimeout(() => g.processGuess("N"), 570);
-setTimeout(() => g.processGuess("O"), 580);
+class Memento {
+  guesses: string[];
+  numberOfErrors: number;
+  constructor(guesses: string[], numberOfErrors: number) {
+    this.guesses = guesses;
+    this.numberOfErrors = numberOfErrors;
+  }
+}
+
+class GameWithUndo extends Game {
+  constructor() {
+    super();
+    this.supportUndo = true;
+    console.log("undo supported!");
+  }
+  public createCheckPoint(): Memento {
+    return new Memento(this.guesses, this.numberOfErrors);
+  }
+
+  public processCheckPoint(checkPoint: Memento): void {
+    this.guesses.length = 0;
+    this.guesses.push(...checkPoint.guesses);
+    this.numberOfErrors = checkPoint.numberOfErrors;
+  }
+}
+
+let g = new Game(); //Originator
+let input = ".";
+// while (!g.gameIsOver()) {
+//   //this main program is the CareTaker
+//   input = prompt("enter a letter");
+//   console.log("Input: ", input);
+//   g.processGuess(input);
+// }
+
+let g2 = new GameWithUndo();
+
+let mementos = [] as Array<Memento>;
+
+while (!g2.gameIsOver()) {
+  //this main program is the CareTaker
+  input = prompt("enter a letter, or '-' to undo");
+  console.log("Input: ", input);
+  if (input === "-" && mementos.length > 1) {
+    mementos.pop();
+    g2.processCheckPoint(mementos[mementos.length - 1]);
+  } else {
+    mementos.push(g2.createCheckPoint());
+    g2.processGuess(input);
+  }
+}
