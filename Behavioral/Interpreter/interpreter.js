991133d83e14c78f5da4841c1d5e92a92e06fc91
= <yevgeller@gmail.com>
2022-05-26 14:48:30 -0500

    Interpreter changes

--- Behavioral/Interpreter/interpreter.js
+++ Behavioral/Interpreter/interpreter.js
@@ -34,6 +34,7 @@
 var Context = /** @class */ (function () {
     function Context(input) {
         this.input = input;
+        this.output = '';
     }
     return Context;
 }());
@@ -48,18 +49,26 @@ var OracleToEFExpression = /** @class */ (function () {
     }
     OracleToEFExpression.prototype.Interpret = function (context) {
         var _this = this;
-        var properties = context.input.split(',');
+        var properties = context.input.split('\n');
         properties.forEach(function (element) {
-            var name = _this.extractName(element);
-            var type = _this.extractType(element);
-            var defaultValue = _this.extractDefaultConstraint(element);
+            if (element.length > 0) {
+                var name_1 = _this.extractName(element);
+                var type = _this.extractType(element);
+                var specifiedDefaultValue = _this.extractDefaultConstraint(element);
+                var determinedDefaultValue = _this.convertDefaultValue(specifiedDefaultValue, type);
+                context.output += "public ".concat(type, " ").concat(_this.convertNameToCamelCase(name_1), " { get; set; }");
+                if (determinedDefaultValue.length > 0)
+                    context.output += " = ".concat(determinedDefaultValue, ";");
+                context.output += '\n';
+            }
         });
+        console.log(context.output);
     };
     OracleToEFExpression.prototype.extractName = function (input) {
         var firstQuote = input.indexOf('"');
         var nextQuote = input.indexOf('"', firstQuote + 1);
         if (firstQuote >= 0 && nextQuote > firstQuote) {
-            return input.substring(firstQuote, input.length - nextQuote);
+            return input.substring(firstQuote + 1, nextQuote);
         }
         return 'NO_NAME_FOUND';
     };
@@ -87,8 +96,33 @@ var OracleToEFExpression = /** @class */ (function () {
     OracleToEFExpression.prototype.extractDefaultConstraint = function (input) {
         var defaultConstraintStartsAt = input.indexOf(' DEFAULT ');
         if (defaultConstraintStartsAt < 0)
-            return null;
+            return '';
+        return input.substring(defaultConstraintStartsAt, input.length);
+    };
+    OracleToEFExpression.prototype.convertDefaultValue = function (input, determinedDataType) {
+        if (determinedDataType === 'string')
+            return 'string.Empty';
+        if (input.trim().length === 0)
+            return '';
+        if (determinedDataType === 'int') {
+            var ret = input.replace('DEFAULT', '');
+            return ret.substring(0, ret.indexOf(','));
+        }
+        if (determinedDataType === 'bool')
+            return input.indexOf('0') >= 0 ? 'false' : 'true';
         return '';
+    };
+    OracleToEFExpression.prototype.convertNameToCamelCase = function (input) {
+        var _this = this;
+        var parts = input.split('_');
+        parts.forEach(function (p, i) { return parts[i] = _this.camelCaseString(p); });
+        return parts.join('_');
+    };
+    OracleToEFExpression.prototype.camelCaseString = function (input) {
+        return input[0] + input.substring(1, input.length).toLocaleLowerCase();
     };
     return OracleToEFExpression;
 }());
+var c = new Context("\n\"ID\" NUMBER(15,0), \n    \"EMPLOYEE_CODE\" VARCHAR2(6 BYTE), \n    \"COMPANY_ID\" NUMBER(15,0), \n    \"FIRST_NAME\" VARCHAR2(50 BYTE),\n    \"LAST_NAME\" VARCHAR2(50 BYTE),\n    \"IS_ACTIVE\" CHAR(1 BYTE) = '1',\n    \"HIRE_DATE\" TIMESTAMP (6)");
+var oef = new OracleToEFExpression();
+oef.Interpret(c);
--- Behavioral/Interpreter/interpreter.ts
+++ Behavioral/Interpreter/interpreter.ts
@@ -37,6 +37,7 @@ class Context {
     output: string;
     constructor(input: string) {
         this.input = input;
+        this.output = '';
     }
 }
 
@@ -46,25 +47,28 @@ abstract class Expression {
 
 class OracleToEFExpression implements Expression {
     Interpret(context: Context): void {
-        let properties = context.input.split(',');
-
+        let properties = context.input.split('\n');
         properties.forEach(element => {
-            let name = this.extractName(element)
-            let type = this.extractType(element)
-            let specifiedDefaultValue = this.extractDefaultConstraint(element);
-            let determinedDefault = this.convertDefaultValue(specifiedDefaultValue, type);
-            context.output += `public ${type} ${name} { get; set; }`;
-            //commit, print out each line
-            //figure out eslint setting on the other computer
-            //set it only on this project
+            if (element.length > 0) {
+                let name = this.extractName(element);
+                let type = this.extractType(element)
+                let specifiedDefaultValue = this.extractDefaultConstraint(element);
+                let determinedDefaultValue = this.convertDefaultValue(specifiedDefaultValue, type);
+                context.output += `public ${type} ${this.convertNameToCamelCase(name)} { get; set; }`;
+                if (determinedDefaultValue.length > 0)
+                    context.output += ` = ${determinedDefaultValue};`
+                context.output += '\n'
+            }
         });
+
+        console.log(context.output)
     }
 
     extractName(input: string): string {
         let firstQuote = input.indexOf('"');
         let nextQuote = input.indexOf('"', firstQuote + 1)
         if (firstQuote >= 0 && nextQuote > firstQuote) {
-            return input.substring(firstQuote, input.length - nextQuote)
+            return input.substring(firstQuote + 1, nextQuote)
         }
         return 'NO_NAME_FOUND'
     }
@@ -96,15 +100,41 @@ class OracleToEFExpression implements Expression {
     }
 
     convertDefaultValue(input: string, determinedDataType: string): string {
+
+        if (determinedDataType === 'string')
+            return 'string.Empty'
         if (input.trim().length === 0)
             return '';
-        if (determinedDataType === 'string')
-            return 'string.Empty;'
-        if (determinedDataType === 'int')
-            return input;
+        if (determinedDataType === 'int') {
+            let ret = input.replace('DEFAULT', '');
+            return ret.substring(0, ret.indexOf(','))
+        }
         if (determinedDataType === 'bool')
             return input.indexOf('0') >= 0 ? 'false' : 'true';
 
         return '';
     }
+
+    convertNameToCamelCase(input: string): string {
+        let parts = input.split('_');
+        parts.forEach((p, i) => parts[i] = this.camelCaseString(p));
+        return parts.join('_');
+    }
+
+    camelCaseString(input: string): string {
+        return input[0] + input.substring(1, input.length).toLocaleLowerCase()
+    }
 }
+
+let c = new Context(`
+"ID" NUMBER(15,0), 
+    "EMPLOYEE_CODE" VARCHAR2(6 BYTE), 
+    "COMPANY_ID" NUMBER(15,0), 
+    "FIRST_NAME" VARCHAR2(50 BYTE),
+    "LAST_NAME" VARCHAR2(50 BYTE),
+    "IS_ACTIVE" CHAR(1 BYTE) = '1',
+    "HIRE_DATE" TIMESTAMP (6)`);
+
+
+let oef = new OracleToEFExpression();
+oef.Interpret(c)
